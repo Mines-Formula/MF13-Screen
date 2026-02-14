e043b6e0584acca36d34fdd6ab4e855a2c7377bd
Noah Slayton <tuco3k@gmail.com>
2026-02-14 09:19:30 -0800

    Fixed Compiler Issue

--- include/nextion.h
+++ include/nextion.h
@@ -52,7 +52,7 @@ private:
     static uint8_t brakeTempPrev;
     static float batteryVoltage;
     static uint16_t engineRPM;
-    static unit16_t rpm;
+    static uint16_t displayRPM;
     static float lambda;
     static int8_t gear;
     static uint16_t prevmph;
